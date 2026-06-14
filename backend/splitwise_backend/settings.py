2064c2cc338d910551ee33808392877a9b750d3d
madhav <madhavpruthi@icloud.com>
2026-06-15 00:42:33 +0530

    chore: configure STATIC_ROOT for static files collection

--- backend/splitwise_backend/settings.py
+++ backend/splitwise_backend/settings.py
@@ -174,6 +174,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
